64bedd23754166a50d5f3b3b9cd9ee4fd7d538d6
Denys Skvortsov <48939274+skvortsovden@users.noreply.github.com>
2025-08-01 16:00:50 +0200

    Fix deployment configuration: add SQLAlchemy init, datetime imports, and specific package versions

--- config.py
+++ config.py
@@ -1,18 +1,20 @@
 import os
+from datetime import datetime, time
+from dotenv import load_dotenv
 
-STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
-STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
-STRAVA_REDIRECT_URI = "http://127.0.0.1:5555/callback"
+load_dotenv()
+
+STRAVA_CLIENT_ID = os.environ.get('STRAVA_CLIENT_ID', 'your_client_id')
+STRAVA_CLIENT_SECRET = os.environ.get('STRAVA_CLIENT_SECRET', 'your_client_secret')
+STRAVA_REDIRECT_URI = os.environ.get('STRAVA_REDIRECT_URI', 'http://localhost:5555/callback')
 
 CLUB_CONFIGS = {
-    'Rotterdam Ukrainian Running Club': {
-        'days': ['Sunday'],
-        'time_window': {
-            'start': '10:30',
-            'end': '12:30'
-        }
-    },
-    # Add more clubs as needed
+    'Rotterdam': {
+        'days': [6],  # Sunday = 6 (Monday = 0)
+        'start_time': datetime.strptime('10:30', '%H:%M').time(),
+        'end_time': datetime.strptime('12:30', '%H:%M').time(),
+        'city': 'Rotterdam'
+    }
 }
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://strava:strava_pass@localhost:5432/strava_board'
